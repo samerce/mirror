ee40fa3a782435fbe5999b75243f84033f11fd62
Samer Chahine <samerce@gmail.com>
2015-08-10 20:01:44 -0400

    Updated gulp deploy so that it doesn't expose AWS credentials.

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -3,12 +3,13 @@
 var gulp = require('gulp');
 var conf = require('./conf');
 var awsPublish = require('gulp-awspublish');
+var awsCred = require('../aws-credentials.json');
 
 gulp.task('deploy', ['build'], function() {
   // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
   var publisher = awsPublish.create({
-    accessKeyId: 'AKIAJZ336ZQ7MSYPYBQA',
-    secretAccessKey: '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',
+    accessKeyId: awsCred.accessKeyId,
+    secretAccessKey: awsCred.secretAccessKey,
     params: {
       Bucket: 'samerchahine.com'
     }
